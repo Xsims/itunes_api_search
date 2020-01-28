0a41990111e376fb07dd2ffc01a14e148cdb16ec
Simon g <romeo.prosecco@gmail.com>
2020-01-28 17:24:11 +0100

    Change design of grid view and details screen (Hero Animation, Collapsing Toolbar...)

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -2,24 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:itunes_api_search/models/product.dart';
 
 class DetailScreen extends StatelessWidget {
-
   // Declare a field that holds the Product.
   final Product product;
   final Image image;
+  final int index;
 
   // In the constructor, require a Product.
-  DetailScreen({Key key, @required this.product, this.image}) : super(key: key);
+  DetailScreen({Key key, @required this.product, this.image, this.index})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // Use the Product to create the UI.
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.trackName),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(product.collectionName),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 400.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Hero(
+                    child: Text(product.trackName,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        )),
+                    tag: 'name_hero$index',
+                  ),
+                  background: Hero(
+                    tag: 'image_hero$index',
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: image.image,
+                          colorFilter: new ColorFilter.mode(
+                              Colors.black.withOpacity(0.4), BlendMode.darken),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  )),
+            ),
+          ];
+        },
+        body: Center(
+          child: Text("Sample Text"),
+        ),
       ),
     );
   }
--- lib/screens/search_results.dart
+++ lib/screens/search_results.dart
@@ -9,8 +9,8 @@ import 'details.dart';
 
 Future<ApiResult> fetchApiResult(http.Client client, String keyword) async {
   final keywordWithoutSpace = keyword.replaceAll(RegExp(' '), '+');
-  final response =
-      await client.get('https://itunes.apple.com/search?term=${keywordWithoutSpace.toLowerCase()}');
+  final response = await client.get(
+      'https://itunes.apple.com/search?term=${keywordWithoutSpace.toLowerCase()}');
   if (response.statusCode == 200) {
     // If server returns an OK response, parse the JSON.
     return ApiResult.fromJson(json.decode(response.body));
@@ -25,30 +25,31 @@ class MyHomePage extends StatelessWidget {
   String searchKeyword = 'jack johnson';
 
   MyHomePage({Key key, this.title}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: TextField(
-            controller: TextEditingController.fromValue(new TextEditingValue(text: title)),
-            style: TextStyle(fontSize: 15),
-            onTap: (){
-              Navigator.pop(context);
-            },
-          ),
-        ),
-        body: FutureBuilder<ApiResult>(
-          future: fetchApiResult(http.Client(), title),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) print(snapshot.error);
-            // Display circular progress while data hasn't been fetched
-            return snapshot.hasData
-                ? ProductsList(apiResult: snapshot.data)
-                : Center(child: CircularProgressIndicator());
+      appBar: AppBar(
+        title: TextField(
+          controller: TextEditingController.fromValue(
+              new TextEditingValue(text: title)),
+          style: TextStyle(fontSize: 15),
+          onTap: () {
+            Navigator.pop(context);
           },
         ),
-      );
+      ),
+      body: FutureBuilder<ApiResult>(
+        future: fetchApiResult(http.Client(), title),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+          // Display circular progress while data hasn't been fetched
+          return snapshot.hasData
+              ? ProductsList(apiResult: snapshot.data)
+              : Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
   }
 }
 
@@ -61,23 +62,55 @@ class ProductsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          mainAxisSpacing: 3,
+          crossAxisSpacing: 3,
           crossAxisCount: 2,
         ),
         itemCount: apiResult.resultCount,
         itemBuilder: (context, index) {
-          return ListTile(
+          return GridTile(
             // Download and display image with the thumbnail's url
-            leading: Image.network(apiResult.products[index].artworkUrl100),
-            title: Text(apiResult.products[index].trackName),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      DetailScreen(product: apiResult.products[index], image: Image.network(apiResult.products[index].artworkUrl100),),
+            child: new InkResponse(
+              child: Hero(
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image:
+                          Image.network(apiResult.products[index].artworkUrl100)
+                              .image,
+                      colorFilter: new ColorFilter.mode(
+                          Colors.black.withOpacity(0.4), BlendMode.darken),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-              );
-            },
+                tag: 'image_hero$index',
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailScreen(
+                      product: apiResult.products[index],
+                      image: Image.network(
+                          apiResult.products[index].artworkUrl100),
+                      index: index,
+                    ),
+                  ),
+                );
+              },
+            ),
+            footer: Hero(
+              child: Text(
+                apiResult.products[index].trackName,
+                style: TextStyle(color: Colors.white),
+              ),
+              tag: 'name_hero$index',
+            ),
+            header: Text(
+              apiResult.products[index].kind,
+              style: TextStyle(color: Colors.white),
+            ),
           );
         });
   }
